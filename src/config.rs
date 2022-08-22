54659dc506607d82ea7c3c3a3c001a360d3c664f
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-08-22 18:34:50 +0430

    Add new options and add auto config file location finding

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,6 @@
+use dirs;
 use serde_derive::Deserialize;
+use std::env::consts::OS;
 
 #[derive(Deserialize)]
 pub struct Config {
@@ -11,10 +13,17 @@ pub struct Config {
     pub total_swap: bool,
     pub used_swap: bool,
     pub cpu_count: bool,
+    pub os_name: bool,
+    pub os_version: bool,
+    pub kernel_version: bool,
+    pub host_name: bool,
+    pub user_name: bool,
+    pub up_time: bool,
     pub ascii: String,
 }
 
 pub fn read_config() -> Config {
+    // Default config.
     let config = Config {
         ram_unit: 'K',
         swap_unit: 'K',
@@ -24,22 +33,52 @@ pub fn read_config() -> Config {
         free_swap: true,
         total_swap: true,
         used_swap: true,
+        os_name: true,
+        os_version: true,
+        kernel_version: true,
+        host_name: true,
+        user_name: true,
+        up_time: true,
+
         cpu_count: true,
         ascii: "".to_string(),
     };
 
-    let file = std::fs::read_to_string("/home/artin/.config/rufetch/rufetch.toml");
-    if file.is_ok() {
-        match toml::from_str(&file.unwrap()) {
-            Ok(value) => return value,
-            Err(_) => {
-                println!("The structure of your config file is not correct.");
-                println!("Using default config.");
-                return config;
-            }
-        };
+    // This variable for storing config file.
+    let file: Result<String, std::io::Error>;
+    let home_dir_addr = dirs::home_dir(); // Home directory address.
+    if home_dir_addr.is_some() {
+        // Trying to open and read config file in windows and other operating systems.
+        if OS == "windows" {
+            file = std::fs::read_to_string(format!(
+                "{}{}",
+                home_dir_addr.unwrap().display(),
+                "\\AppData\\Local\\rufetch\\rufetch.toml"
+            ));
+        } else {
+            file = std::fs::read_to_string(format!(
+                "{}{}",
+                home_dir_addr.unwrap().display(),
+                "/.config/rufetch/rufetch.toml"
+            ));
+        }
+
+        if file.is_ok() {
+            match toml::from_str(&file.unwrap()) {
+                Ok(value) => return value,
+                Err(_) => {
+                    eprintln!(
+                        "The structure of your config file is not correct.\nUsing default config."
+                    );
+                    return config;
+                }
+            };
+        } else {
+            eprintln!("Can't open/read config file");
+            return config;
+        }
     } else {
-        println!("Can't open/read config file");
+        eprintln!("Can't find your home directory address.");
         return config;
     }
 }
